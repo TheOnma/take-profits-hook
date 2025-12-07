a6a5215c05e29b4d92d75be1ecd14b68d11c0d4c
Precious Onma Adekwu <preciousadekwu1@gmail.com>
2025-12-07 14:58:05 +0100

    Created Take Profits Hook Test Contract

---
+++ test/TakeProfitsHook.t.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+// Foundry libraries
+import {Test} from "forge-std/Test.sol";
+
+import {Deployers} from "@uniswap/v4-core/test/utils/Deployers.sol";
+import {PoolSwapTest} from "v4-core/test/PoolSwapTest.sol";
+import {MockERC20} from "solmate/src/test/utils/mocks/MockERC20.sol";
+import {SwapParams, ModifyLiquidityParams} from "v4-core/types/PoolOperation.sol";
+
+import {PoolManager} from "v4-core/PoolManager.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {SwapParams, ModifyLiquidityParams} from "v4-core/types/PoolOperation.sol";
+
+import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
+import {Currency, CurrencyLibrary} from "v4-core/types/Currency.sol";
+import {StateLibrary} from "v4-core/libraries/StateLibrary.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {TickMath} from "v4-core/libraries/TickMath.sol";
+
+import {TakeProfitsHook} from "../src/TakeProfitsHook.sol";
+
+contract TakeProfitsHookTest is Test, Deployers {
+    using StateLibrary for IPoolManager;
+
+    // The two currencies (tokens) from the pool
+    Currency token0;
+    Currency token1;
+
+    TakeProfitsHook hook;
+
+    function setUp() public {
+        // TODO
+    }
+}
