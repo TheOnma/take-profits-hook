6d535b2aae821417c4c5e9feab9e40b40738608f
Precious Onma Adekwu <preciousadekwu1@gmail.com>
2025-12-08 15:08:06 +0100

    Updated function to test orderExecute_zeroForOne

--- test/TakeProfitsHook.t.sol
+++ test/TakeProfitsHook.t.sol
@@ -166,5 +166,24 @@ contract TakeProfitsHookTest is Test, Deployers, ERC1155Holder {
             PoolSwapTest.TestSettings({takeClaims: false, settleUsingBurn: false});
 
         // Conduct the swap - `afterSwap` should also execute our placed order
+        swapRouter.swap(key, params, testSettings, ZERO_BYTES);
+
+        // Check that the order has been executed
+        // by ensuring no amount is left to sell in the pending orders
+        uint256 pendingTokensForPosition = hook.pendingOrders(key.toId(), tick, zeroForOne);
+        assertEq(pendingTokensForPosition, 0);
+
+        // Check that the hook contract has the expected number of token1 tokens ready to redeem
+        uint256 orderId = hook.getOrderId(key, tickLower, zeroForOne);
+        uint256 claimableOutputTokens = hook.claimableOutputTokens(orderId);
+        uint256 hookContractToken1Balance = token1.balanceOf(address(hook));
+        assertEq(claimableOutputTokens, hookContractToken1Balance);
+
+        // Ensure we can redeem the token1 tokens
+        uint256 originalToken1Balance = token1.balanceOf(address(this));
+        hook.redeem(key, tick, zeroForOne, amount);
+        uint256 newToken1Balance = token1.balanceOf(address(this));
+
+        assertEq(newToken1Balance - originalToken1Balance, claimableOutputTokens);
     }
 }
